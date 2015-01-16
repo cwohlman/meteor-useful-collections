df7193275efa4e424d3afb6add6a6a0abb8053bd
Joshua Ohlman <codewriterohs@gmail.com>
2015-01-16 11:59:23 -0600

    Finish basic hooks.

--- tests/hooks.js
+++ tests/hooks.js
@@ -9,47 +9,144 @@ var Books = new UsefulCollection(_books);
 if (Meteor.isServer) {
   log.remove({});
 
-  // Books.hooks({
-  //   "before.insert": function (doc) {
-  //     log.insert({
-  //       method: "insert"
-  //       , where: "before"
-  //       , logField: doc.logField
-  //     });
-  //   }
-  // });
-
-  UsefulCollection.hook(Books, "before.insert", function (doc) {
-    log.insert({
-      logField: doc.logField
-    });
+  Books.hooks({
+    "before.insert": function (doc) {
+      log.insert({
+        method: "insert"
+        , where: "before"
+        , logField: doc.logField
+      });
+    }
+    , "before.update": function (query, modifier) {
+      if (modifier && modifier.$set && modifier.$set.logField){
+        log.insert({
+          method: "update"
+          , where: "before"
+          , logField: modifier.$set.logField
+        });
+      }
+    }
+    , "before.remove": function (query) {
+      log.insert({
+        method: "remove"
+        , where: "before"
+        , logField: query._id
+      });
+    }
+    , "after.insert": function (doc) {
+      log.insert({
+        method: "insert"
+        , where: "after"
+        , logField: doc.logField
+      });
+    }
+    , "after.update": function (query, modifier) {
+      if (modifier && modifier.$set && modifier.$set.logField){
+        log.insert({
+          method: "update"
+          , where: "after"
+          , logField: modifier.$set.logField
+        });
+      }
+    }
+    , "after.remove": function (query) {
+      log.insert({
+        method: "remove"
+        , where: "after"
+        , logField: query._id
+      });
+    }
   });
 }
 
-if (Meteor.isClient) {
-  Tinytest.addAsync('UsefulCollections - hooks - before insert', function (test, done) {
-    Books.insert({logField: "a"}, function () {
-      test.equal(log.find({logField: "a"}).count(), 1);
+var logField;
+
+Tinytest.addAsync('UsefulCollections - hooks - before insert', function (test, done) {
+  logField = Random.id();
+  Books.insert({logField: logField}, function () {
+    test.equal(log.find({logField: logField, method: "insert", where: "before"}).count(), 1);
+    done();
+  });
+});
+
+Tinytest.add('UsefulCollections - hooks - after insert', function (test) {
+  test.equal(log.find({
+    logField: logField
+    , where: 'after'
+    , method: 'insert'
+  }).count(), 1);
+});
+
+Tinytest.addAsync('UsefulCollections - hooks - before update', function (test, done) {
+  logField = Random.id();
+  var doc = Books.findOne({logField: {$exists: true}});
+  Books.update(doc._id, {$set:{logField: logField}}, function () {
+    test.equal(log.find({logField: logField, method: "update", where: "before"}).count(), 1);
+    done();
+  });
+});
+
+Tinytest.add('UsefulCollections - hooks - after update', function (test) {
+  test.equal(log.find({
+    logField: logField
+    , where: 'after'
+    , method: 'update'
+  }).count(), 1);
+});
+
+var docRemoved;
+
+Tinytest.addAsync('UsefulCollections - hooks - before remove', function (test, done) {
+  logField = Random.id();
+  docRemoved = Books.findOne({logField: {$exists: true}});
+  Books.remove(docRemoved._id, function () {
+    test.equal(log.find({
+      logField: docRemoved._id
+      , method: "remove"
+      , where: "before"
+    }).count(), 1);
+    done();
+  });
+});
+
+Tinytest.add('UsefulCollections - hooks - after remove', function (test) {
+  test.equal(log.find({
+    logField: docRemoved._id
+    , where: 'after'
+    , method: 'remove'
+  }).count(), 1);
+});
+
+if (Meteor.isServer) {
+  Tinytest.addAsync('UsefulCollections - hooks - survives monkey patch', function (test, done) {
+    var original = _books._collection.insert;
+    var patch;
+    logField = Random.id();
+
+    _books._collection.insert = patch = function (doc) {
+      return original.apply(_books._collection, arguments);
+    };
+    Books.hooks({
+      "before.insert": function (doc) {
+        log.insert({
+          logField: doc.logField
+          , method: "mokeypatch"
+          , where: "before"
+        });
+      }
+      , "after.insert": function (doc) {
+        log.insert({
+          logField: doc.logField
+          , method: "mokeypatch"
+          , where: "after"
+        });
+      }
+    });
+    test.equal(_books._collection.insert, patch);
+    Books.insert({logField: logField}, function () {
+      test.equal(log.find({logField: logField, method: "mokeypatch", where: "before"}).count(), 1);
+      test.equal(log.find({logField: logField, method: "mokeypatch", where: "after"}).count(), 1);
       done();
     });
   });
 }
-
-// Tinytest.add('UsefulCollections - hooks - before update', function (test) {
-//   var Books = new UsefulCollection(_books);
-
-//   var hookWasRun = false;
-//   Books.hooks({
-//     "before.update": function () {
-//       hookWasRun = true;
-//     }
-//   });
-
-//   Books.update(Books.findOne()._id, {
-//     $set: {
-//       "before update hook": true
-//     }
-//   });
-  
-//   test.isTrue(hookWasRun);
-// });
